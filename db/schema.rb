7e07a3c0014641f75b7a5c1e50db8660df5b270a
Alen Esteibar <alenesteibar@gmail.com>
2021-06-14 16:11:39 +0200

    Jobs table and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_134506) do
+ActiveRecord::Schema.define(version: 2021_06_14_135057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.string "details"
+    t.string "location"
+    t.date "date"
+    t.integer "duration"
+    t.integer "price"
+    t.integer "rating", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_06_14_134506) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jobs", "users"
 end
