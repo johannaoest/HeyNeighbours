5c4431a534c94de85af52a0a0cf75371ce941d5f
Alen Esteibar <alenesteibar@gmail.com>
2021-06-14 17:39:41 +0200

    Bookings basic controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
   root to: 'pages#home'
-  resources :jobs
+
+  resources :jobs do
+    resources :bookings, only: %i[new create]
+  end
+
+  resources :bookings, except: %i[new create]
 end
