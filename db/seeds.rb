51b9d293bfab7a328eec483eb6c06079f31e8882
lili vincze <lili.vincze@gmail.com>
2021-06-15 15:55:11 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,49 @@ job = Job.new(
   date: "2021. 06. 17.",
   user: User.find(1)
 )
-job.photo.attach(io: file, filename: 'dog.png', content_type: 'image/png')
+job.photo.attach(io: file, filename: 'job.png', content_type: 'image/png')
 job.save!
 
+file = URI.open('https://images.unsplash.com/photo-1588964895597-cfccd6e2dbf9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Z3JvY2VyaWVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+job = Job.new(
+  title: "Grocery shopping tomorrow in the morning",
+  details: "I need somebody to do the shopping for me. I need a lot of things from the supermarket, so preferably I am looking for a strong young man who can carry many bags.",
+  location: "Puerta del Angel",
+  price: 11,
+  duration: 2,
+  date: "2021. 06. 16.",
+  user: User.find(2)
+)
+job.photo.attach(io: file, filename: 'job.png', content_type: 'image/png')
+job.save!
+
+file = URI.open('https://images.unsplash.com/photo-1561382774-7f200b5f5c3c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')
+job = Job.new(
+  title: "Grass cutting",
+  details: "Hello, my garden looks terrible, the grass is super long, I need someone to cut it for me!",
+  location: "Casa de campo, Madrid",
+  price: 30,
+  duration: 5,
+  date: "2021. 06. 16.",
+  user: User.find(3)
+)
+job.photo.attach(io: file, filename: 'job.png', content_type: 'image/png')
+job.save!
+
+file = URI.open('https://images.unsplash.com/photo-1578593195423-df7df9563457?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2V3b3JrfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+job = Job.new(
+  title: "Clean my windows please",
+  details: "I need SOS help, I can't even look through them! I have 12 windows, I'd like to pay 2 euros for each.",
+  location: "Madrid",
+  price: 24,
+  duration: 3,
+  date: "2021. 06. 16.",
+  user: User.find(2)
+)
+job.photo.attach(io: file, filename: 'job.png', content_type: 'image/png')
+job.save!
+
+puts "Created jobs"
 
 # t.bigint "user_id", null: false
 # t.string "title"
