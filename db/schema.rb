84e551bddb6d4d890addfffbb2bfb44b0245af22
Johanna Oesterreicher <johannaoest@hotmail.com>
2021-06-16 15:46:41 +0200

    SetUp Categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_091937) do
+ActiveRecord::Schema.define(version: 2021_06_16_131604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_091937) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "category"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
