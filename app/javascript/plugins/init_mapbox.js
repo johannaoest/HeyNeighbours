6c422dbdb22866570b75ef51d21a09b2b45bb109
Alen Esteibar <alenesteibar@gmail.com>
2021-06-17 22:16:06 +0200

    Map filter

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -26,19 +26,12 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
-const mapFilterBar = () => {
-  const filterBar = document.getElementById('filter');
-  filterBar.addEventListener('change', (event) => {
-    document.getElementById["filter-form"].submit();
-  })
-}
-
 const filterValue = () => {
   const input = document.querySelector('#filter_distance');
   const value = document.getElementById('value');
   console.log('Hola')
   input.addEventListener('input', () => {
-    value.innerHTML = input.value;
+    value.innerHTML = input.value + ' km';
   })
 }
 
