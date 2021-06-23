b12f14bbe89092cb5044f6400f9b2d14e4395e0b
Alen Esteibar <alenesteibar@gmail.com>
2021-06-23 14:38:12 +0200

    Reviews working for both

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,13 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.booking = @booking
     @review.user = current_user
+
+    if current_user == @booking.job.user
+      @review.reviewed_user = @booking.user
+    else
+      @review.reviewed_user = @booking.job.user
+    end
+
     authorize @review
     if @review.save
       redirect_to users_show_path(@review.booking.job.user)
