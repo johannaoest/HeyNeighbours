539903ba8218fc8ee350fe7c2389060944878846
Alen Esteibar <alenesteibar@gmail.com>
2021-06-24 10:24:58 +0200

    Payment in progress

---
+++ db/migrate/20210624082247_add_paid_to_bookings.rb
@@ -0,0 +1,5 @@
+class AddPaidToBookings < ActiveRecord::Migration[6.0]
+  def change
+    add_column :bookings, :paid, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_142352) do
+ActiveRecord::Schema.define(version: 2021_06_24_082247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_142352) do
     t.boolean "pending", default: false
     t.boolean "employer_rated", default: false
     t.boolean "employee_rated", default: false
+    t.boolean "paid", default: false
     t.index ["job_id"], name: "index_bookings_on_job_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
