fc942823f8aee470ceb470df0589cc451ff2d3de
lili vincze <lili.vincze@gmail.com>
2021-06-24 12:01:04 +0200

    message notificationsss

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user
+
+  after_commit :deliver_notification
+
+  def deliver_notification
+    notification = MessageNotification.with(message: self.id)
+    notification.deliver(self.user)
+  end
 end
