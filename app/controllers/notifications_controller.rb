6bdaa3dd90eda927c78a14c18633e6ecb3700c7b
Alen Esteibar <alenesteibar@gmail.com>
2021-06-24 15:31:29 +0200

    Message notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,6 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = policy_scope(current_user.notifications)
+    @notifications = policy_scope(current_user.notifications).order(created_at: :desc)
   end
 end
